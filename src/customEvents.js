ac0950d6f212fbfeee812a56bd0a361a28f8de4c
intertext.no <orstavik77@gmail.com>
2022-06-13 15:32:54 +0200

    drafting some more, next is adding filter

--- src/customEvents.js
+++ src/customEvents.js
@@ -1,3 +1,26 @@
+//str=>obj=>obj => v map
+//todo naive, it doesn't clean itself up perfectly. It can mushroom.
+class StrOOMap {
+  get(Str, o1, o2) {
+    return this[Str]?.get(o1)?.get(o2);
+  }
+
+  has(Str, o1, o2) {
+    return !!this.get(Str, o1, o2);
+  }
+
+  set(Str, o1, o2, v) {
+    const one = this[Str] || (this[Str] = new WeakMap());
+    let two = one.get(o1);
+    !two && one.set(o1, two = new WeakMap());
+    two.set(o2, v);
+  }
+
+  remove(Str, o1, o2) {
+    return this[Str]?.get(o1)?.remove(o2);
+  }
+}
+
 const events = {};
 
 window.customEvents = {};
@@ -7,34 +30,27 @@ customEvents.define = function (str, Class) {
   events[str] = Class;
 };
 
-//ev => el => cb => customEvent;
-const customEventMap = new WeakMap();
-
-function getCustomEventInstance(el, ev, cb) {
-  return customEventMap[ev]?.get(el)?.get(cb);
+function getCustomEvent(name) {
+  //todo here we can add the filter definitions.
+  return events[name];
 }
 
-function hasCustomEventInstance(el, ev, cb) {
-  return !!getCustomEventInstance(el, ev, cb);
-}
-
-function setCustomEvent(el, ev, cb, customEvent) {
-  const one = customElements[ev] || (customElements[ev] = new WeakMap());
-  let two = one.get(el);
-  !two && one.set(el, two = new WeakMap());
-  two.set(cb, customEvent);
-}
-
+//EventType => el => cb => customEvent;
+const customEventInstances = new StrOOMap();
 
 //monkeypatch the add/removeEventListener
 (function (EventTargetOG, addEventListenerOG, removeEventListenerOG) {
   EventTargetOG.prototype.addEventListener = function addEventListener_customEvents(type, cb) {
-    if (type in events) {
-      if (hasCustomEventInstance(this, type, cb))
+    const Definition = getCustomEvent(type);
+    if (Definition) {
+      if (customEventInstances.has(type, this, cb))
         throw "what to do when one event is being activated by several cbs on the same element?";
-      const customEvent = new events[type](this);
-      setCustomEvent(this, type, cb, customEvent);
+      customEventInstances.set(type, this, cb, new Definition(this));
     }
     addEventListenerOG.call(this, type, cb);
+  };
+  EventTargetOG.prototype.removeEventListener = function removeEventListener_customEvents(type, cb) {
+    customEventInstances.remove(type, this, cb)?.destructor();
+    removeEventListenerOG.call(this, type, cb);
   }
 })(EventTarget, addEventListener, removeEventListener);
