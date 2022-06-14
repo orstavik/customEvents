5c8e3beec0525c38332c29242591b78cbddfc821
intertext.no <orstavik77@gmail.com>
2022-06-14 10:48:45 +0200

    babystep

--- src/customEvents.js
+++ src/customEvents.js
@@ -1,3 +1,12 @@
+const events = {};
+
+window.customEvents = {};
+customEvents.define = function (str, Class) {
+  if (str in events)
+    throw str + " is already defined as a custom event.";
+  events[str] = Class;
+};
+
 //str=>obj=>obj => v map
 //todo naive, it doesn't clean itself up perfectly. It can mushroom.
 class StrOOMap {
@@ -21,36 +30,34 @@ class StrOOMap {
   }
 }
 
-const events = {};
-
-window.customEvents = {};
-customEvents.define = function (str, Class) {
-  if (str in events)
-    throw str + " is already defined as a custom event.";
-  events[str] = Class;
-};
-
-function getCustomEvent(name) {
-  //todo here we can add the filter definitions.
-  return events[name];
-}
-
 //EventType => el => cb => customEvent;
 const customEventInstances = new StrOOMap();
+//todo ABC. what i need is just the type+el => customEventInstance + [cb1, cb2, cb3]...
+// when there are no more listeners, then call destructor and remove.
+
+function monkeypatchCustomEventsAdd(OG) {
+  return function addEventListener_customEvents(type, cb, ...args) {
+    const Definition = events[type];
+    if (Definition) {
+      if (customEventInstances.has(type, this, cb))
+        throw "what to do when one event is being activated by several cbs on the same element?"; //todo no see ABC
+      customEventInstances.set(type, this, cb, new Definition(this));
+    }
+    OG.call(this, type, cb, ...args);
+  }
+}
+
+function monkeypatchCustomEventsRemove(OG) {
+  return function removeEventListener_customEvents(type, cb, ...args) {
+    customEventInstances.remove(type, this, cb)?.destructor();
+    OG.call(this, type, cb, ...args);
+  }
+}
 
 //monkeypatch the add/removeEventListener
 (function (EventTargetOG, addEventListenerOG, removeEventListenerOG) {
-  EventTargetOG.prototype.addEventListener = function addEventListener_customEvents(type, cb) {
-    const Definition = getCustomEvent(type);
-    if (Definition) {
-      if (customEventInstances.has(type, this, cb))
-        throw "what to do when one event is being activated by several cbs on the same element?";
-      customEventInstances.set(type, this, cb, new Definition(this));
-    }
-    addEventListenerOG.call(this, type, cb);
-  };
-  EventTargetOG.prototype.removeEventListener = function removeEventListener_customEvents(type, cb) {
-    customEventInstances.remove(type, this, cb)?.destructor();
-    removeEventListenerOG.call(this, type, cb);
-  }
+  EventTargetOG.prototype.addEventListener =
+    monkeypatchCustomEventsAdd(addEventListenerOG);
+  EventTargetOG.prototype.removeEventListener = 
+    monkeypatchCustomEventsRemove(removeEventListenerOG);
 })(EventTarget, addEventListener, removeEventListener);
