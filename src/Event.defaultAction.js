37f00f97bac5f812e3e7794a4e6b625f21dfd968
intertext.no <orstavik77@gmail.com>
2022-06-15 12:27:17 +0200

    fixing rewinding of paths for defaultAction callbacks

--- src/Event.defaultAction.js
+++ src/Event.defaultAction.js
@@ -1,3 +1,18 @@
+//the default action is a method/function that is passed the event with the currentTarget, target, path and composedPath
+// "back set" to the value of when the default action was made.
+function rewindPath(event, currentTarget, target, path, composedPath) {
+  Object.defineProperties(event, {
+    "currentTarget": {value: currentTarget},
+    "target": {value: target},
+    "path": {value: path},
+    "composedPath": {
+      value: function () {
+        return composedPath
+      }
+    },
+  });
+}
+
 (function monkeyDefaultAction(Event) {
   const defaultActions = new WeakMap();
   Object.defineProperty(Event.prototype, "defaultAction", {
@@ -9,9 +24,14 @@
         throw "default actions can only be set once";
       if (!(v instanceof Function))
         throw "defaultActions must be a callable function";
+      const currentTarget = this.currentTarget;
+      const target = this.target;
+      const path = this.path;
+      const composedPath = this.composedPath();
+      //these values will work with closed and open shadowDoms alike. As they are read at the similar hierarchy position during propagation.
       defaultActions.set(this, v);
       const audio = document.createElement("audio");
-      audio.onratechange = _ => this.defaultPrevented || v();
+      audio.onratechange = _ => this.defaultPrevented || v(rewindPath(this, currentTarget, target, path, composedPath));
       audio.playbackRate = 2;
     }
   });
