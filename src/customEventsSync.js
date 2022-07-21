454457827a4e476576d4e570fa153f31428b41bf
intertext.no <orstavik77@gmail.com>
2022-07-21 09:36:36 +0200

    setting up test set draft

--- src/customEventsSync.js
+++ src/customEventsSync.js
@@ -1,16 +1,17 @@
-//the default action is a method/function that is passed the event with the currentTarget, target, path and composedPath
-// "back set" to the value of when the default action was made.
-function rewindPath(event, currentTarget, target, path, composedPath) {
+//mutate the target, currentTarget, path, composedPath of an Event object.
+function rewind(event, currentTarget, target, path, composedPath) {
   Object.defineProperties(event, {
     "currentTarget": {value: currentTarget},
     "target": {value: target},
     "path": {value: path},
-    "composedPath": {
-      value: function () {
-        return composedPath
-      }
-    },
+    "composedPath": {value: composedPath}
   });
+}
+
+function nextTick(cb){
+  const audio = document.createElement("audio");
+  audio.onratechange = cb;
+  audio.playbackRate = 2;
 }
 
 function monkeyDefaultAction(Event) {
@@ -20,19 +21,16 @@ function monkeyDefaultAction(Event) {
       return !!defaultActions.get(this);
     },
     set(v) {
-      if (defaultActions.has(this))
-        throw "default actions can only be set once";
       if (!(v instanceof Function))
         throw "defaultActions must be a callable function";
-      const currentTarget = this.currentTarget;
-      const target = this.target;
-      const path = this.path;
-      const composedPath = this.composedPath();
-      //these values will work with closed and open shadowDoms alike. As they are read at the similar hierarchy position during propagation.
+      if (defaultActions.has(this))
+        return;
       defaultActions.set(this, v);
-      const audio = document.createElement("audio");
-      audio.onratechange = _ => this.defaultPrevented || v(rewindPath(this, currentTarget, target, path, composedPath));
-      audio.playbackRate = 2;
+      const current = this.currentTarget;
+      const target = this.target;
+      const path = this.path;              //path and composedPath will work with closed and open shadowDoms,
+      const composed = this.composedPath();//as they are read at the same position as the callback.
+      nextTick(_ => this.defaultPrevented || rewind(this, current, target, path, () => composed) || v(this));
     }
   });
 }
