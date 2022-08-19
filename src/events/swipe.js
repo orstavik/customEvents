3734ec14b03875656e864971c999920e2cd1e02d
intertext.no <orstavik77@gmail.com>
2022-08-19 13:08:55 +0200

    babystep

--- src/events/swipe.js
+++ src/events/swipe.js
@@ -2,6 +2,33 @@ function mouseOutOfBounds(trigger) {
   return trigger.clientY < 0 || trigger.clientX < 0 || trigger.clientX > window.innerWidth || trigger.clientY > window.innerHeight;
 }
 
+class SwipeEvent extends PointerEvent {
+  #options;
+
+  constructor(type, options) {
+    super(type, {bubbles: true, composed: true});
+    this.#options = options;
+  }
+
+  get direction() {
+    const direction = this.#options.swipeDistY > 0 ? "top" : "down";
+    return direction + "-" + (this.#options.swipeDistX > 0 ? "right" : "left");
+  }
+
+  //step 1 would be to make it in degrees? turn two sets of coordinates into a degree
+  //step 2 mark
+  //this is a getter.
+  //         north/up = > 300 || north < 60
+  //         south/down = > 120 && < 240
+  //         east/right = > 120 && < 240
+  //         west/left = > 120 && < 240
+  //         north-east / north /??
+  //         horisontal ()narrower focus
+  //         vertical
+  //getter for acceleration
+  //getter for speed
+}
+
 export function createSwipe({minDuration = 50, minDistance = 50, direction} = {}) {
   return class Swipe {
     constructor(ownerElement) {
@@ -46,35 +73,15 @@ export function createSwipe({minDuration = 50, minDistance = 50, direction} = {}
     }
 
     onMouseUp(e) {
+      //todo check for minDuration or maxDuration of the swipe here.
+
       let swipeStartX = this.sequence[0].x - this.owner.offsetLeft;
       let swipeStartY = this.sequence[0].y - this.owner.offsetTop;
       let swipeDistX = swipeStartX - e.x;
       let swipeDistY = swipeStartY - e.y;
-      let direction;
-
-      //step 1 would be to make it in degrees? turn two sets of coordinates into a degree
-      //step 2 mark
-      //this is a getter.
-      //         north/up = > 300 || north < 60
-      //         south/down = > 120 && < 240
-      //         east/right = > 120 && < 240
-      //         west/left = > 120 && < 240
-      //         north-east / north /??
-      //         horisontal ()narrower focus
-      //         vertical
-      //getter for acceleration
-      //getter for speed
-
-      if (Math.abs(swipeDistX) > Math.abs(swipeDistY) && Math.abs(swipeDistX) > minDistance)
-        direction = swipeDistX > 0 ? "right" : "left";
-      else if (Math.abs(swipeDistY) > minDistance)
-        direction = swipeDistY > 0 ? "top" : "down";
-      else
+      if (!(Math.abs(swipeDistX) > Math.abs(swipeDistY) && Math.abs(swipeDistX) > minDistance || Math.abs(swipeDistY) > minDistance))
         return;
-      // const swipe = new e.constructor("swipe", {bubbles: true, composed: true});
-      const swipe = new CustomEvent("swipe", {bubbles: true, composed: true, detail: {direction: direction}});
-
-      e.defaultAction = _ => this.owner.dispatchEvent(swipe);
+      e.defaultAction = _ => this.owner.dispatchEvent(new SwipeEvent("swipe", {swipeDistX, swipeDistY}));
       this.reset();
     }
 
@@ -88,6 +95,7 @@ export function createSwipe({minDuration = 50, minDistance = 50, direction} = {}
       this.owner.addEventListener("selectstart", this.onSelectstartListener);
     }
 
+    //todo should we revert to pointer instead of mouse events? probably? why not?
     stopSequence() {
       this.owner.addEventListener("mousedown", this.mousedownInitialListener);
       this.owner.removeEventListener("mousedown", this.mousedownSecondaryListener);
