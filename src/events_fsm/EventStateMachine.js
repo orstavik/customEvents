e20e79e4e6549d5175314c912c3c79db7c1283c4
intertext.no <orstavik77@gmail.com>
2022-08-26 13:14:49 +0200

    comments

--- src/events_fsm/EventStateMachine.js
+++ src/events_fsm/EventStateMachine.js
@@ -1,3 +1,21 @@
+/*
+ * Rules for EventStateMachine:
+ * 1. The event controller must be in one of the given states.
+ * 2. The event controller starts in the first listed state.
+ * 3. Each state is a list of transitions to the next state(s).
+ * 4. A transition is made up of the nextState's name, the action (function), and the trigger event's name and target.
+ * 5. When the trigger event occurs, it is passed to the action function.
+ *    The action function is successful if it does not return false.
+ * 6. If the action is successful, no other state change triggers will run for this event for this state machine.
+ *    This means that the sequence of the transitions for each state is important as the first successful action will win.
+ * 7. The action/transition and state change is performed sync during event propagation.
+ * 8. Side-effects should be one of two things:
+ *    A. Set/remove/update protected attribute. The protected attributes reflect the state of the event controller.
+ *    B. dispatch new event(s). Event dispatches should be done async:
+ *       B1. successful steps in the anticipated direction are commonly added as the default action on the trigger event.
+ *       B2. cancellation steps are commonly added as new macro tasks in the event loop using nextTick (not as default actions).
+ */
+
 function makeListener(seen, machine, owner, state, nextState, action) {
   return function (e) {
     if (seen.has(e))
@@ -23,5 +41,12 @@ export class EventStateMachine {
     //starting the first listeners
     for (let [listener, , , event, target = owner] of machine[Object.keys(machine)[0]])
       target.addEventListener(event, listener);
+  }
+
+  destructor(){
+    //todo remove the event listeners on the machine[state] currently active.
+    // for (let [listener, , , event, target = owner] of machine[currentState])
+    //   target.removeEventListener(event, listener);
+    //todo we need to have the machine and the currentState as this.xyz. If not, we can't cleanup.
   }
 }
