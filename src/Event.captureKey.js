5ed8f487ef34247921a98f93c5f9310e32e49ad1
intertext.no <orstavik77@gmail.com>
2022-09-20 10:03:04 +0200

    adding captureKey test

--- src/Event.captureKey.js
+++ src/Event.captureKey.js
@@ -2,7 +2,7 @@ export function monkeyEventCapture(Event) {
   //1. Make sure that the counter starts after any lingering/frozen <meta capture="number"> from frozen html code
   let count = 0;
   for (let el of document.head.querySelectorAll(`:scope > meta[capture]`))
-    count = Math.max(count, ...[...el.getAttribute("capture")].split(" "));
+    count = Math.max(count, ...el.getAttribute("capture").split(" "));
   count++;
   //2. eventToKey creates an immutable, read-only captureKey id for each event object
   let eventToKey = new WeakMap();
